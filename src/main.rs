1b2295cb70d9a53772d9ad7fe6edaf42bc93b01c
Vitaly Sulimov <v.sulimov.dev@imap.cc>
2025-06-23 18:05:01 +0300

    Add home website implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(actix_files::Files::new("/", "./static").index_file("index.html"))
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
+}
+
+#[cfg(test)]
+mod tests {
+    use actix_files;
+    use actix_web::{App, test};
+
+    #[actix_web::test]
+    async fn test_index() {
+        let app = test::init_service(
+            App::new().service(actix_files::Files::new("/", "./static").index_file("index.html")),
+        )
+        .await;
+        let req = test::TestRequest::get().uri("/").to_request();
+        let resp = test::call_service(&app, req).await;
+        assert!(resp.status().is_success());
+        let body = test::read_body(resp).await;
+        let body_str = str::from_utf8(body.as_ref()).expect("Response body is not valid UTF-8");
+        assert!(body_str.contains("Vitaly Sulimov"));
+        assert!(body_str.contains(r#"<a href="https://git.vsulimov.com">git</a>"#));
+        assert!(body_str.contains(r#"<a href="https://maven.vsulimov.com">maven</a>"#));
+        assert!(body_str.contains(r#"<a href="https://instagram.com/open.climb">Instagram</a>"#));
+    }
 }
